6201824a2155187086966b52e57ba3c14fe9e1c4
Spindui <rowanjtanner@gmail.com>
2025-09-23 17:02:24 +0100

    "Added .gitignore file for book data and main.py function"

---
+++ main.py
@@ -0,0 +1,9 @@
+def get_book_text(filepath):
+    with open(filepath) as f:
+        file_contents = f.read()
+
+def main()
+    print(f"{get_book_text(books/frankenstien.txt)}")
+
+if __name__ == "__main__":
+    main()
