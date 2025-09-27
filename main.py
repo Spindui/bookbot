c9e351f4125bd37ff0adbd87d4be853b2af3e920
Spindui <rowanjtanner@gmail.com>
2025-09-27 13:02:04 +0100

    made main.py take an input on the command line and work on any book

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from stats import *
+import sys
 
 if __name__ == "__main__":
-    list_of_indvidiual_char_dict = sort_char_counts(get_char_counts(get_book_text('books/frankenstein.txt')))
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    list_of_indvidiual_char_dict = sort_char_counts(get_char_counts(get_book_text(sys.argv[1])))
 
 
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {sys.argv[1]}")
     print("----------- Word Count ----------")
-    print_word_count("books/frankenstein.txt")
+    print_word_count(sys.argv[1])
     for item in list_of_indvidiual_char_dict:
         ch = item["char"]
         counts = item["num"]
