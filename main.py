3c7477782326b882a75bb292b3f2a2113cef1127
Spindui <rowanjtanner@gmail.com>
2025-09-27 12:42:55 +0100

    added code in main.py that runs sort_char_counts and prints them out nicely

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from stats import *
 
 if __name__ == "__main__":
-    print_word_count('books/frankenstein.txt')
-    list_of_inv_nums = get_char_counts(get_book_text('books/frankenstein.txt'))
-    print(list_of_inv_nums)
+    list_of_indvidiual_char_dict = sort_char_counts(get_char_counts(get_book_text('books/frankenstein.txt')))
+
+
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print_word_count("books/frankenstein.txt")
+    for item in list_of_indvidiual_char_dict:
+        ch = item["char"]
+        counts = item["num"]
+        if ch.isalpha():
+            print(f"{ch}: {counts}")
+    print("============= END ===============")
