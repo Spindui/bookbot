03363c8646e7b7f969792488fb899d8674572d8b
Spindui <rowanjtanner@gmail.com>
2025-09-27 12:34:51 +0100

    added functions to return a list of dicts with thier char and num

--- stats.py
+++ stats.py
@@ -3,12 +3,12 @@ def get_word_count(file_contents: str):
     num_words = len(words)
     return num_words
 
-def get_book_text(filepath):
+def get_book_text(filepath: str):
     with open(filepath) as f:
         file_contents = f.read()
     return file_contents
 
-def print_word_count(filepath):
+def print_word_count(filepath: str):
     file_contents = get_book_text(filepath)
     num_words = get_word_count(file_contents)       
     print(f"Found {num_words} total words")
@@ -22,3 +22,18 @@ def get_char_counts(file_contents: str):
         else:
              num_of_char_dict[char] = 1
     return num_of_char_dict
+
+def sort_char_counts(char_dictionary: dict):
+    items = create_individual_dict_for_list(char_dictionary)
+    items.sort(reverse= True, key=sort_on)
+    return items
+
+def sort_on(items):
+    return items["num"]
+
+def create_individual_dict_for_list(char_dictionary: dict)-> list:
+    list_of_dicts = []
+    for k, v in char_dictionary.items():
+        list_of_dicts.append({"char": k, "num": v})
+    return list_of_dicts
+     
